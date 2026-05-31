072e44a3e01f092197d0560411848f3be8f3f74e
Vahe Sevachyan <vscode13@gmail.com>
2026-05-31 08:15:16 -0700

    refactored HeroSection Component styles
    Extracted styles into variables

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,17 +1,28 @@
 <template>
   <section class="bg-gray-700 py-20 mb-4">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-      <div class="text-center">
-        <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-          {{ title }}
-        </h1>
-        <p class="my-4 text-xl text-white">{{ subtitle }}</p>
-      </div>
+    <div :class="heroStyles">
+      <h1 :class="h1Styles">
+        {{ title }}
+      </h1>
+      <p class="my-4 text-xl text-white">{{ subtitle }}</p>
     </div>
   </section>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+const heroStyles = ref(
+  `max-w-7xl mx-auto 
+   px-4 sm:px-6 lg:px-8 
+   flex flex-col 
+   items-center text-center`,
+);
+
+const h1Styles = ref(`
+  text-4xl font-extrabold 
+  text-white sm:text-5xl 
+  md:text-6xl`);
+
 defineProps({
   title: {
     type: String,
